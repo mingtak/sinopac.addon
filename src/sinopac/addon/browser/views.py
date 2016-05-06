878a559801a1a393f8fd1390f13c61e43d4d8b40
Andy Fang <andy@mingtak.com.tw>
2016-05-06 18:05:23 +0800

    add questions.pt, and define macro, mobile

--- src/sinopac/addon/browser/views.py
+++ src/sinopac/addon/browser/views.py
@@ -2,6 +2,7 @@ from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 #from zope.component import getMultiAdapter
 from plone import api
+import random
 from DateTime import DateTime
 import transaction
 
@@ -17,6 +18,32 @@ class Leaving(BrowserView):
 class Measures(BrowserView):
     """ Measures View
     """
+
+class Questions(BrowserView):
+    """ Questions View
+    """
+
+    index = ViewPageTemplateFile('template/questions.pt')
+
+    def __call__(self):
+        context = self.context
+        request = self.request
+        response = request.response
+        catalog = context.portal_catalog
+
+        if not (request.get('HTTP_REFERER', '').endswith('questions-start') != request.get('HTTP_REFERER', '').endswith('questions')):
+            response.redirect('/')
+            return
+
+        self.questions = []
+        for keyword in ['A', 'B', 'C', 'D', 'E']:
+            self.questions.append(random.choice(catalog({'Type':'Question', 'Subject':keyword})))
+
+        return self.index()
+
+
+
+
 
 class QuestionsConfirm(BrowserView):
     """ Questions-confirm View
